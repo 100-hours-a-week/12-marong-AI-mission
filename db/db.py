fa24429e0daa0f3b7e984b85004f66aa1cd3eac7
KIMSS <kcg199510@gmail.com>
2025-06-12 13:33:45 +0900

    refactor: 코드 리팩토링

--- db/db.py
+++ db/db.py
@@ -13,8 +13,8 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = "sqlite:///./missions.db"
-# DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = "sqlite:///./missions.db"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 if "sqlite" in DATABASE_URL:
     engine = create_engine(
